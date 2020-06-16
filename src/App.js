0d14dadae9996b99f3401ec9abc1cfa0255b7a9c
Nick t <ns.tahani@gmail.com>
2020-06-16 14:40:25 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,6 @@ import React from 'react'
 
 export function App () {
   return (
-    <h1 style={{margin: '1vw'}}>🎉</h1>
+    <h1 style={{margin: '1vw'}}>building a simple UI for daily covid-19 statistics...</h1>
   )
 }
